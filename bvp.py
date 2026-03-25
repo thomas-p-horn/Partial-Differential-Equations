a37803f6fb1e5c7393f4cd168f61bfa86d5ddea7
Thomas P Horn <tph259@gmail.com>
2026-03-25 23:28:23 +0000

    poisson.py changed to bvp.py. Most of assignment done, just need to get graphs going

--- bvp.py
+++ bvp.py
@@ -5,7 +5,7 @@ import argparse
 import time
 
 @njit
-def gauss_seidel(phi, rho, omega, max_steps, tolerance):
+def gauss_seidel_3D(phi, rho, omega, max_steps, tolerance):
     L = phi.shape[0]
     for n in range(max_steps):
         phi_old = phi.copy()
@@ -29,6 +29,28 @@ def gauss_seidel(phi, rho, omega, max_steps, tolerance):
     
     return phi
 
+@njit
+def gauss_seidel_2D(A, J, omega, max_steps, tolerance):
+    L = A.shape[0]
+    for n in range(max_steps):
+        A_old = A.copy()
+
+        for i in range(1, L-1):
+            for j in range(1, L-1):
+                new_val = (
+                    A[i+1,j] + A[i-1,j] +
+                    A[i,j+1] + A[i,j-1] +
+                    J[i,j]
+                ) / 6.0
+
+                # Successive Over-Relaxation (SOR)
+                A[i,j] = (1 - omega) * A_old[i,j] + omega * new_val
+
+        if np.max(np.abs(A - A_old)) <= tolerance:
+            print(f"Converged in {n} steps")
+            break
+    
+    return A
 
 
 
@@ -75,14 +97,15 @@ class PoissonSolver:
         return self.phi
 
     def solve_gauss_seidel(self):
-        self.phi = gauss_seidel(self.phi, self.rho, self.omega, self.max_steps, self.tolerance)
+        self.phi = gauss_seidel_3D(self.phi, self.rho, self.omega, self.max_steps, self.tolerance)
         return self.phi
     
-    def compute_electric_field(self):
+    def electric_field(self):
         Ex = np.zeros_like(self.phi)
         Ey = np.zeros_like(self.phi)
         Ez = np.zeros_like(self.phi)
 
+        # Grad
         Ex[1:-1,:,:] = -(self.phi[2:,:,:] - self.phi[:-2,:,:]) / 2
         Ey[:,1:-1,:] = -(self.phi[:,2:,:] - self.phi[:,:-2,:]) / 2
         Ez[:,:,1:-1] = -(self.phi[:,:,2:] - self.phi[:,:,:-2]) / 2
@@ -93,19 +116,23 @@ class PoissonSolver:
         mid = self.L // 2
 
         plt.figure()
-        plt.title("Potential (midplane slice)")
-        plt.imshow(self.phi[:,:,mid], origin='lower')
+        plt.title(r"Electrostatic Potential $\phi$ (midplane slice)")
+        plt.xlabel(r"x")
+        plt.ylabel(r"y")
+        plt.imshow(self.phi[:,:,mid], origin='lower', cmap='hot')
         plt.colorbar()
-        plt.show()
+        plt.savefig("Electrostatic Potential.png", dpi=300, bbox_inches='tight')
+        plt.close()
 
     def plot_field(self):
-        Ex, Ey, _ = self.compute_electric_field()
+        Ex, Ey, _ = self.electric_field()
         mid = self.L // 2
 
         plt.figure()
         plt.title("Electric Field (midplane)")
         plt.quiver(Ex[:,:,mid], Ey[:,:,mid])
-        plt.show()
+        plt.savefig("Electric Field.png", dpi=300, bbox_inches='tight')
+        plt.close()
 
 
 
@@ -117,76 +144,77 @@ class MagneticSolver():
         self.max_steps = max_steps
         self.omega = omega
         
-        self.A = np.zeros((L, L, L))
+        self.Az = np.zeros((L, L))
         self.set_current()
 
     def set_current(self, current=1.):
-        self.J = np.zeros((self.L, self.L, self.L))
+        self.Jz = np.zeros((self.L, self.L))
         mid = self.L // 2
-        self.J[mid, mid, :] = current
+        self.Jz[mid, mid] = current
     
     def residual(self, phi_old):
-        return np.max(np.abs(self.A - phi_old))
+        return np.max(np.abs(self.Az - phi_old))
 
     def solve_jacobi(self):
 
         for n in range(self.max_steps):
-            A_old = self.A.copy()
+            Az_old = self.Az.copy()
 
-            self.A[1:-1,1:-1,1:-1] = (
-                A_old[2:,1:-1,1:-1] +
-                A_old[:-2,1:-1,1:-1] +
-                A_old[1:-1,2:,1:-1] +
-                A_old[1:-1,:-2,1:-1] +
-                A_old[1:-1,1:-1,2:] +
-                A_old[1:-1,1:-1,:-2] +
-                self.J[1:-1,1:-1,1:-1]
-            ) / 6.0
+            self.Az[1:-1,1:-1] = (
+                Az_old[2:,1:-1] +
+                Az_old[:-2,1:-1] +
+                Az_old[1:-1,2:] +
+                Az_old[1:-1,:-2] +
+                self.Jz[1:-1,1:-1]
+            ) / 4.0
 
-            if self.residual(A_old) < self.tolerance:
+            if self.residual(Az_old) < self.tolerance:
                 print(f"Converged in {n} steps")
                 break
 
-        return self.A
+        return self.Az
 
     def solve_gauss_seidel(self):
-        self.A = gauss_seidel(self.A, self.J, self.omega, self.max_steps, self.tolerance)
-        return self.A
+        self.Az = gauss_seidel_2D(self.Az, self.Jz, self.omega, self.max_steps, self.tolerance)
+        return self.Az
 
-    def compute_electric_field(self):
-        Ex = np.zeros_like(self.A)
-        Ey = np.zeros_like(self.A)
-        Ez = np.zeros_like(self.A)
+    def magnetic_field(self):
+        Bx = np.zeros_like(self.Az)
+        By = np.zeros_like(self.Az)
 
-        Ex[1:-1,:,:] = -(self.A[2:,:,:] - self.A[:-2,:,:]) / 2
-        Ey[:,1:-1,:] = -(self.A[:,2:,:] - self.A[:,:-2,:]) / 2
-        Ez[:,:,1:-1] = -(self.A[:,:,2:] - self.A[:,:,:-2]) / 2
+        # Curl (All of A is in A_z, other cross term derivatives vanish)
+        Bx[1:-1, :] = (self.Az[2:, :] - self.Az[:-2, :]) / 2   # ∂Az/∂y
+        By[:, 1:-1] = -(self.Az[:, 2:] - self.Az[:, :-2]) / 2
 
-        return Ex, Ey, Ez
+        return Bx, By
 
     def plot_midplane(self):
-        mid = self.L // 2
 
         plt.figure()
-        plt.title("Potential (midplane slice)")
-        plt.imshow(self.A[:,:,mid], origin='lower')
+        plt.title(r"Magnetic Potential $A_z$ (midplane slice)")
+        plt.xlabel(r"x")
+        plt.ylabel(r"y")
+        plt.imshow(self.Az[:,:], origin='lower', cmap='hot')
         plt.colorbar()
-        plt.show()
+        plt.savefig("Magnetic Potential.png", dpi=300, bbox_inches='tight')
+        plt.close()
 
     def plot_field(self):
-        Ex, Ey, _ = self.compute_electric_field()
-        mid = self.L // 2
+        Bx, By = self.magnetic_field()
 
         plt.figure()
-        plt.title("Electric Field (midplane)")
-        plt.quiver(Ex[:,:,mid], Ey[:,:,mid])
-        plt.show()
+        plt.title(r"Magnetic Field $B$ (midplane)")
+        plt.xlabel(r"x")
+        plt.ylabel(r"y")
+        plt.quiver(Bx[:,:], By[:,:])
+        plt.savefig("Magnetic Field.png", dpi=300, bbox_inches='tight')
+        plt.close()
 
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="Runs a solver for the Poisson equation (or for the magnetic field equation)")
-    parser.add_argument("-p", "--problem", help="Which problem to solve. 'poisson' or 'magnetic'. Default='poisson'.", type=str, default='poisson')
+    parser = argparse.ArgumentParser(description="Runs a solver for the Poisson equation or for the magnetic field equation")
+    parser.add_argument("-p", "--problem", help="Which problem to solve. 'electrostatic' or 'magnetic'. Default='electrostatic'.", type=str, default='electrostatic')
     parser.add_argument("-a", "--algorithm", help="Algorithm used to solve problem. 'jacobi' or 'gauss-seidel'. Default='jacobi'.", type=str, default='jacobi')
     parser.add_argument("-l", "--length", help="LxLxL size of lattice. Default=100.", type=int, default=100)
     parser.add_argument("-t", "--tolerance", help="Tolerance requirement to terminate solver. (Solver runs until highest value for difference between each step is below tolerance.) Default=1e-5.", type=float, default=1e-5)
@@ -202,7 +230,7 @@ if __name__ == "__main__":
     max_steps = args.max
     omega = args.omega
 
-    if problem.lower() == 'poisson':
+    if problem.lower() == 'electrostatic':
         solver = PoissonSolver(
             L,
             tolerance,
